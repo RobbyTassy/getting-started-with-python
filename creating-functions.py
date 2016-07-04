90d349296fc087ebeff51fb78eac2de6d20aef06
Robert Tassy <Rtass001@fiu.edu>
2016-07-04 15:29:47 -0400

    functions

---
+++ creating-functions.py
@@ -0,0 +1,17 @@
+def happyBirthdayEmily():
+    print ("Happy Birthday to you!")
+    print ("Happy Birthday to you")
+    print ("Happy Birthday, dear Emily!")
+    print ("Happy Birthday to you! \n")
+    print ("Hope you have a wonderful Birthday!, Emily \n")
+5
+happyBirthdayEmily()
+
+def happyBirthdayJack():
+    print ("Happy Birthday to you!")
+    print ("Happy Birthday to you")
+    print ("Happy Birthday, dear Jack!")
+    print ("Happy Birthday to you! \n")
+    print ("Hope you have a wonderful Birthday!, Jack \n")
+
+happyBirthdayJack()
