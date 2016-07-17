243cc36271a474bca66f33bb10dbcf092b760ddd
Robert Tassy <Rtass001@fiu.edu>
2016-07-16 20:18:39 -0400

    if statements

---
+++ if-statement.py
@@ -0,0 +1,11 @@
+# This is a simple if statement
+
+# turns users input into a float (a decimal number)
+weight = float(input("How many pounds does your suitecase weigh? "))
+
+if weight > 50:
+    print("There is a $25 charge for luggade that heavy")
+    print("Thank you for your business")
+else:
+    print("Have a safe trip!")
+        
