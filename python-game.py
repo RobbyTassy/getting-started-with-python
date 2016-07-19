8a0f0086d6436817305442d7cb84add71d09d7ba
Robert Tassy <Rtass001@fiu.edu>
2016-07-18 22:38:13 -0400

    name input - pyton game

--- python-game.py
+++ python-game.py
@@ -5,20 +5,20 @@ import random
 # defining variables
 
 n = random.randint(1,10)
-
+name = raw_input("Enter your name: ")
 guess = int(input("Enter an integer from 1 to 10: "))
 
 # Do a loop
 
 while n  != "guess":
     if guess < n:
-        print ("Your guess is too low")
+        print ("Your guess is too low, " + name + "!")
         guess = int(input("Enter an integer from 1 to 10: "))
     elif guess > n:
-        print ("Your guess is too high")
+        print ("Your guess is too high, " + name + "!")
         guess = int(input("Enter an integer from 1 to 10: "))
     else:
-        print ("You guessed correctly! It was %d!" % (guess))        
+        print ("You guessed correctly " + name + "! " + "It was %d!" % (guess))        
         break
 
     
