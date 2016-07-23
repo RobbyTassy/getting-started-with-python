9e52ce132902bd79ee6f3027a7a7aae1239bb84d
Robert Tassy <Rtass001@fiu.edu>
2016-07-23 11:32:48 -0400

    manipulating data in lists

--- advanced-lists.py
+++ advanced-lists.py
@@ -22,3 +22,18 @@ nums.insert (0, 10)
 nums.insert(0,[1,2,3])
 # print (nums)
 # >> [[1, 2, 3], 10, 1, 3, 7, 8, 9, 4]
+
+# makes an array from a range of numbers 0 to 10
+nums = range(10)
+# print nums
+# >> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for each value in array 0 to 10, increase it by 2nd power
+nums = [x**2 for x in range(10)]
+# print nums
+# >> [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# for each value in array 0 to 10, increase it by 2nd power
+# if it's remainder is 0 when divided by 2 (or if even number)
+nums = [x**2 for x in range(10) if x % 2 ==0]
+print nums
