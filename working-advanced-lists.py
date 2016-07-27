6630b87989cfec64a677d9deab6a5b8ef22a60c5
Robert Tassy <Rtass001@fiu.edu>
2016-07-26 21:41:25 -0400

    big fix

--- working-advanced-lists.py
+++ working-advanced-lists.py
@@ -1,29 +1,32 @@
 class Node(object):
 
-    def __init__(self, d, n=None):
+    def __init__ (self, d, n = None):
         self.data = d
         self.next_node = n
 
-    def get_next(self):
+    def get_next (self):
         return self.next_node
 
-    def set_next(self, n):
+    def set_next (self, n):
         self.next_node = n
 
-    def set_data(self, d):
+    def get_data (self):
+        return self.data
+
+    def set_data (self, d):
         self.data = d
 
-class LinkedList(object):
+class LinkedList (object):
 
-    def __init__(self, r=None):
+    def __init__(self, r = None):
         self.root = r
         self.size = 0
 
-    def get_size(self):
+    def get_size (self):
         return self.size
 
     def add (self, d):
-        new_node = None (d, self.root)
+        new_node = Node (d, self.root)
         self.root = new_node
         self.size += 1
 
@@ -37,28 +40,26 @@ class LinkedList(object):
                     prev_node.set_next(this_node.get_next())
                 else:
                     self.root = this_node.get_next()
-                    self.size -= 1
-                    return True #data removed
+                self.size -= 1
+                return True     # data removed
             else:
                 prev_node = this_node
                 this_node = this_node.get_next()
-                return False # if it does not find any data
+        return False  # data not found
 
     def find (self, d):
         this_node = self.root
-        while this.node:
+        while this_node:
             if this_node.get_data() == d:
                 return d
             else:
-                this.node = this.node.get_next()
-            return None
+                this_node = this_node.get_next()
+        return None
 
-
-    myList = LinkedList()
-    # myList.add(5)
-    # myList.add(8)
-    # myList.add(2)
-    # myList.remove(8)
-
-    # print (myList.remove(12))
-    # print (myList.find(5))
+myList = LinkedList()
+myList.add(5)
+myList.add(8)
+myList.add(12)
+myList.remove(8)
+print(myList.remove(12))
+print(myList.find(5))
