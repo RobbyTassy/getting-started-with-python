0670e3bb9962a405b196b32718079ae171f95584
Robert Tassy <Rtass001@fiu.edu>
2016-07-29 20:47:22 -0400

    intersection: finds common data types between arrays

--- sets-and-dictionaries.py
+++ sets-and-dictionaries.py
@@ -4,11 +4,11 @@ items = {"arrow", "spear", "arrow", "arrow", "rock"}
 
 # print set
 
-print (items)
-print (len(items))
+# print (items)
+# print (len(items))
 
-if "rock" in items:
-    print ("rock found!")
+# if "rock" in items:
+#     print ("rock found!")
 
 #Create an empty set
 # resets your array list
@@ -19,17 +19,20 @@ items.add("cat")
 items.add("dog")
 items.add("fish")
 
-print (items)
+# print (items)
 
 numbers1 = {1, 3, 5, 7}
 numbers2 = {1, 3}
 
 # issubset method runs parameter within called statement to check if
 # the parameter lives inside called statement
-if numbers2.issubset(numbers1):
-    print("Is a subset")
+# if numbers2.issubset(numbers1):
+#     print("Is a subset")
 
 # similarly, numbers 2 is being checked to see if numbers 1
 # lives within in
-if numbers1.issuperset (numbers2):
-    print("Is a superset")
+# if numbers1.issuperset (numbers2):
+#     print("Is a superset")
+
+# finds the common numbers in the array that numbers1 had wiht numbers2
+print (numbers1.intersection(numbers2))
