a3d6b368304eb09bad862f4dc67962b33d7580f4
Robert Tassy <Rtass001@fiu.edu>
2016-08-03 22:22:25 -0400

    looping techniques

--- loops.py
+++ loops.py
@@ -18,3 +18,47 @@ if i != -1:
     print ("string found")
 else:
     print ("string not found")
+
+value = "cat picture is cat picture"
+
+# start with this value
+
+location = -1
+
+# loop while true
+
+while True:
+    # advance location by -1
+    location = value.find ("picture", location + 1)
+
+    # break if not found
+    if location == -1:
+        break
+
+    # display the location
+    print (location)
+
+value = "cat picture is cat picture"
+
+i = value.rfind("picture")
+print (i)
+
+# get the rightmost index within this range of characters
+
+b = value.rfind("picture", 0, i - 1)
+print (b)
+
+value = "cat picture is cat picture"
+
+# start with length of string
+
+i = len(value)
+
+while True:
+    # find rightmost string in this range
+    i = value.rfind("picture", 0, i)
+
+    # check for not found
+    if i == -1:
+        break
+    print (i)
